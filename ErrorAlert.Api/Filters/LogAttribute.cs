fbaab0f2b5d528168fe5d91ab99b6781a77b83ca
Diyorbek <diyorbek2506@gmail.com>
2024-01-11 18:02:48 +0500

    add

---
+++ ErrorAlert.Api/Filters/LogAttribute.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace ErrorAlert.Api.Filters
+{
+    public class LogAttribute : IActionFilter
+    {
+        private string ipAddress;
+        private int port;
+        private HttpRequest? request;
+        private HttpResponse response;
+        private string? methodType;
+        private int? companyId;
+        private int? userId;
+        private bool isCoorporateOrg = false;
+        private string? controllerName;
+        private string? actionName;
+        private string? requestBody;
+        private string? requestHeader;
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var httpContect = context.HttpContext;
+            var user = httpContect?.User;
+
+            controllerName = httpContect?.GetRouteValue("controller")?.ToString();
+            actionName = httpContect?.GetRouteValue("action")?.ToString();
+
+            request = httpContect?.Request;
+            methodType = request?.Method;
+
+            requestBody = JsonSerializer.Serialize(context.ActionArguments);
+            requestHeader = JsonSerializer.Serialize(request.Headers);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var response = context.HttpContext.Response;
+            Exception? exception = context?.Exception;
+
+            try
+            {
+                var result = context?.Result;
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
